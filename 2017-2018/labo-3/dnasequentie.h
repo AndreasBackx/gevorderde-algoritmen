d3d2cecda8d747b754b19e74833bd469b521172f
MartenBE <martijn.saelens@protonmail.com>
2017-10-19 00:13:15 +0200

    Oplossing aangepast aan labo en top down dynamic programming oplossing toegevoegd

--- 2017-2018/labo-3/dnasequentie.h
+++ 2017-2018/labo-3/dnasequentie.h
@@ -3,6 +3,9 @@
 #include <vector>
 #include <algorithm>
 
+#include <iostream>
+#include <iomanip>
+
 // Levenshtein distance
 // http://users.monash.edu/~lloyd/tildeAlgDS/Dynamic/Edit/
 // https://secweb.cs.odu.edu/~zeil/cs361/web/website/Lectures/styles/pages/editdistance.html
@@ -22,8 +25,13 @@ public:
     DNASequentie(DNASequentie&& ander) = default;
     DNASequentie& operator=(DNASequentie&& ander) = default;
 
-    int d(const std::string& andere) const;
-    int d_volgens_conventie(const std::string& andere) const;
+    int d_bottom_up(const DNASequentie& andere) const;
+    int d_bottom_up_volgens_conventie(const DNASequentie& andere) const;
+    int DNASequentie::d_top_down_volgens_conventie(const DNASequentie& andere);
+
+private:
+
+    int DNASequentie::d_top_down_volgens_conventie(const DNASequentie& andere, std::vector<std::vector<int>>& afstanden, int van, int naar);
 };
 
 DNASequentie::DNASequentie(const std::string& inhoud)
@@ -47,7 +55,7 @@ DNASequentie::DNASequentie(std::string&& inhoud)
 //    I 3
 //    L 4
 //
-int DNASequentie::d_volgens_conventie(const std::string& andere) const
+int DNASequentie::d_bottom_up_volgens_conventie(const DNASequentie& andere) const
 {
     const size_t VAN_MATRIX_GROOTTE = size() + 1;
     const size_t NAAR_MATRIX_GROOTTE = andere.size() + 1;
@@ -85,6 +93,67 @@ int DNASequentie::d_volgens_conventie(const std::string& andere) const
     return afstanden[VAN_MATRIX_GROOTTE - 1][NAAR_MATRIX_GROOTTE - 1];
 }
 
+
+int DNASequentie::d_top_down_volgens_conventie(const DNASequentie& andere)
+{
+    const size_t VAN_MATRIX_GROOTTE = size() + 1;
+    const size_t NAAR_MATRIX_GROOTTE = andere.size() + 1;
+
+    std::vector<std::vector<int>> afstanden(
+        VAN_MATRIX_GROOTTE,
+        std::vector<int>(NAAR_MATRIX_GROOTTE, -1)
+    );
+
+    int afstand = d_top_down_volgens_conventie(andere, afstanden, (VAN_MATRIX_GROOTTE - 1), (NAAR_MATRIX_GROOTTE - 1));
+
+    /*for (const std::vector<int>& naar : afstanden)
+    {
+        for (int afstand : naar)
+        {
+            std::cout << std::setw(3) << afstand;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;*/
+
+    return afstand;
+}
+
+int DNASequentie::d_top_down_volgens_conventie(
+    const DNASequentie& andere,
+    std::vector<std::vector<int>>& afstanden,
+    int van,
+    int naar
+)
+{
+    if (afstanden[van][naar] < 0)
+    {
+        if (van == 0)
+        {
+            afstanden[van][naar] = naar;
+        }
+        else if (naar == 0)
+        {
+            afstanden[van][naar] = van;
+        }
+        else
+        {
+            int verandering = d_top_down_volgens_conventie(andere, afstanden, (van - 1), (naar - 1));
+            if ((*this)[van - 1] != andere[naar - 1])
+            {
+                verandering++;
+            }
+
+            int toevoegen = (1 + d_top_down_volgens_conventie(andere, afstanden, van, (naar - 1)));
+            int verwijderen = (1 + d_top_down_volgens_conventie(andere, afstanden, (van - 1), naar));
+
+            afstanden[van][naar] = std::min({verandering, toevoegen, verwijderen});
+        }
+    }
+
+    return afstanden[van][naar];
+}
+
 // Correcte oplossing omgekeerd volgens labo
 //
 // Vanachter naar voor: bv. zeil -> trails
@@ -96,7 +165,7 @@ int DNASequentie::d_volgens_conventie(const std::string& andere) const
 //                  3 E
 //                  4 Z
 //
-int DNASequentie::d(const std::string& andere) const
+int DNASequentie::d_bottom_up(const DNASequentie& andere) const
 {
     const size_t VAN_MATRIX_GROOTTE = size() + 1;
     const int NAAR_MATRIX_GROOTTE = andere.size() + 1;
--- 2017-2018/labo-3/main.cpp
+++ 2017-2018/labo-3/main.cpp
@@ -7,142 +7,8 @@
 #include <cassert>
 #include <vector>
 #include <iomanip>
-
-std::vector<DNASequentie> lees_bestand(const std::string& bestandsnaam)
-{
-    std::vector<DNASequentie> lijnen;
-
-    std::ifstream bestand(bestandsnaam);
-
-    std::string lijn;
-    while(bestand >> lijn)
-    {
-        lijnen.push_back(DNASequentie{lijn});
-    }
-
-    bestand.close();
-
-    return lijnen;
-}
-
-void test_labo()
-{
-    std::vector<DNASequentie> human_dna{lees_bestand("human.dna")};
-    std::cout << "Mens:" << std::endl;
-    for (size_t i = 0; i < human_dna.size(); i++)
-    {
-        std::cout << i << ": " << human_dna[i] << std::endl;
-    }
-    std::cout << std::endl;
-
-    std::vector<DNASequentie> chimp_dna{lees_bestand("chimp.dna")};
-    std::cout << "Aap:" << std::endl;
-    for (size_t i = 0; i < chimp_dna.size(); i++)
-    {
-        std::cout << i << ": " << chimp_dna[i] << std::endl;
-    }
-    std::cout << std::endl;
-
-    constexpr int AANTAL_OPLOSSINGEN = 75;
-    constexpr int OPLOSSINGEN[AANTAL_OPLOSSINGEN][3] =
-    {   // human, chimp, oplossing
-        {0, 69 ,24},
-        {1, 63 ,10},
-        {2, 27 ,25},
-        {3, 45 ,26},
-        {4, 46 ,12},
-        {5, 25 ,30},
-        {6, 61 ,10},
-        {7, 57 ,12},
-        {8, 74 ,35},
-        {9, 59 ,9},
-        {10, 43 ,26},
-        {11, 47 ,25},
-        {12, 33 ,36},
-        {13, 66 ,23},
-        {14, 18 ,12},
-        {15, 40 ,7},
-        {16, 44 ,11},
-        {17, 6 ,11},
-        {18, 32 ,17},
-        {19, 2 ,23},
-        {20, 54 ,23},
-        {21, 62 ,13},
-        {22, 4 ,13},
-        {23, 15 ,21},
-        {24, 48 ,26},
-        {25, 12 ,21},
-        {26, 21 ,25},
-        {27, 19 ,34},
-        {28, 8 ,20},
-        {29, 3 ,17},
-        {30, 9 ,20},
-        {31, 73 ,18},
-        {32, 1 ,22},
-        {33, 22 ,23},
-        {34, 11 ,24},
-        {35, 24 ,10},
-        {36, 13 ,5},
-        {37, 5 ,23},
-        {38, 65 ,12},
-        {39, 7 ,25},
-        {40, 35 ,24},
-        {41, 60 ,28},
-        {42, 10 ,8},
-        {43, 49 ,22},
-        {44, 29 ,21},
-        {45, 67 ,28},
-        {46, 16 ,7},
-        {47, 64 ,28},
-        {48, 71 ,28},
-        {49, 50 ,11},
-        {50, 37 ,27},
-        {51, 56 ,30},
-        {52, 17 ,5},
-        {53, 38 ,26},
-        {54, 31 ,25},
-        {55, 23 ,3},
-        {56, 68 ,14},
-        {57, 36 ,19},
-        {58, 53 ,12},
-        {59, 51 ,23},
-        {60, 39 ,7},
-        {61, 42 ,20},
-        {62, 55 ,20},
-        {63, 52 ,24},
-        {64, 30 ,24},
-        {65, 70 ,24},
-        {66, 14 ,29},
-        {67, 34 ,17},
-        {68, 26 ,13},
-        {69, 0 ,14},
-        {70, 20 ,5},
-        {71, 58 ,20},
-        {72, 72 ,28},
-        {73, 41 ,21},
-        {74, 28 ,18}
-    };
-
-    for (int i = 0; i < AANTAL_OPLOSSINGEN; i++)
-    {
-        const int HUMAN_INDEX = OPLOSSINGEN[i][0];
-        const int CHIMP_INDEX = OPLOSSINGEN[i][1];
-        const int OPLOSSING = OPLOSSINGEN[i][2];
-        const int AFSTAND = human_dna[HUMAN_INDEX].d(chimp_dna[CHIMP_INDEX]);
-        const int CONVENT_AFSTAND = human_dna[HUMAN_INDEX].d_volgens_conventie(chimp_dna[CHIMP_INDEX]);
-
-        std::cout << "Check " << std::setw(2) << i << " voor mens " << std::setw(2) << HUMAN_INDEX
-                  << " en aap " << std::setw(2) << CHIMP_INDEX << " geeft (" << std::setw(2) << AFSTAND
-                  << " & " << std::setw(2) << CONVENT_AFSTAND << ") en verwacht " << std::setw(2) << OPLOSSING;
-
-        assert(human_dna[HUMAN_INDEX].d(chimp_dna[CHIMP_INDEX]) == OPLOSSING);
-        assert(human_dna[HUMAN_INDEX].d_volgens_conventie(chimp_dna[CHIMP_INDEX]) == OPLOSSING);
-
-        std::cout << " [ OK ]" << std::endl;
-    }
-
-    std::cout << "Test DNA OK" << std::endl;
-}
+#include <limits>
+#include <chrono>
 
 void test_labo_uitbreiding()
 {
@@ -175,20 +41,59 @@ void test_labo_uitbreiding()
         std::cout << " [ OK ]" << std::endl;
     }
 
-    assert(bm.min_aantal_flessen(13, test_reeks) == 4);
-    assert(bm.min_aantal_flessen(3, test_reeks) == 1);
-    assert(bm.min_aantal_flessen(5, test_reeks) == 2);
-    assert(bm.min_aantal_flessen(8, test_reeks) == 2);
-    assert(bm.min_aantal_flessen(10, test_reeks) == 2);
-    assert(bm.min_aantal_flessen(12, test_reeks) == 3);
-    assert(bm.min_aantal_flessen(1, test_reeks) == 1);
-
     std::cout << "Test bottelmachine OK" << std::endl;
 }
 
 int main()
 {
-    test_labo();
+    std::vector<DNASequentie> aap_dna;
+
+    std::ifstream bestand{"chimp.dna"};
+    DNASequentie aap;
+
+    while (bestand >> aap)
+    {
+        aap_dna.push_back(aap);
+    }
+
+    bestand.close();
+    bestand.clear();
+
+    bestand.open("human.dna");
+    DNASequentie mens;
+
+    auto start_time = std::chrono::steady_clock::now();
+
+    int i = 0;
+    while (bestand >> mens)
+    {
+        size_t min_index = 0;
+        int min_afstand = std::numeric_limits<int>::max();
+
+        for (int j = 0; j < aap_dna.size(); j++)
+        {
+            int afstand = mens.d_bottom_up_volgens_conventie(aap_dna[j]);
+            // assert(afstand == mens.d_bottom_up(aap_dna[j]));
+            // assert(afstand == mens.d_top_down_volgens_conventie(aap_dna[j]));
+
+            if (afstand < min_afstand)
+            {
+                min_afstand = afstand;
+                min_index = j;
+            }
+        }
+
+        std::cout << "Menselijk " << i << " komt overeen met chimpansees " << min_index << " met afstand " << min_afstand << std::endl;
+
+        i++;
+    }
+
+    auto finish_time = std::chrono::steady_clock::now();
+
+    bestand.close();
+
+    std::cout << "Verstreken tijd voor DNA vergelijkingen: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish_time - start_time).count() << " ms" << std::endl;
+
     test_labo_uitbreiding();
 
     return 0;

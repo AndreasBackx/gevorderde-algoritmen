b84c0eb72a64483b0ed2c0a2919b738b642389d7
MartenBE <martijn.saelens@protonmail.com>
2018-02-03 09:18:07 +0100

    Alle testen voor graaf toegevoegd

--- libraries/graaf/graaf_test.cpp
+++ libraries/graaf/graaf_test.cpp
@@ -1,6 +1,8 @@
 #include "graaf.h"
 #include "gtest/gtest.h"
 
+#include <string>
+
 TEST(graaf_test, constructor)
 {
     const int SIZE = 20;
@@ -143,3 +145,82 @@ TEST(graaf_test, wis)
     EXPECT_EQ(knoop_nr2, 1);
     EXPECT_EQ(verbind_nr, 0);
 }
+
+TEST(graaf_met_takdata_test, constructor)
+{
+    const int SIZE = 20;
+
+    GraafMetTakdata<ONGERICHT, int> graaf{SIZE};
+
+    EXPECT_EQ(graaf.aantalKnopen(), SIZE);
+    EXPECT_EQ(graaf.aantalVerbindingen(), 0);
+}
+
+TEST(graaf_met_takdata_test, constructor_with_default_parameter)
+{
+    GraafMetTakdata<ONGERICHT, int> graaf;
+
+    EXPECT_EQ(graaf.aantalKnopen(), 0);
+    EXPECT_EQ(graaf.aantalVerbindingen(), 0);
+}
+
+TEST(graaf_met_takdata_test, voeg_verbinding_toe_no_parameter)
+{
+    GraafMetTakdata<ONGERICHT, std::string> graaf{4};
+
+    graaf.voegVerbindingToe(0, 1);
+
+    ASSERT_NE(graaf.geefTakdata(0, 1), nullptr);
+    EXPECT_EQ((*graaf.geefTakdata(0, 1)), std::string());
+}
+
+TEST(graaf_met_takdata_test, voeg_verbinding_toe)
+{
+    GraafMetTakdata<ONGERICHT, std::string> graaf{4};
+
+    const std::string content = "Dit is een test";
+
+    graaf.voegVerbindingToe(0, 1, content);
+
+    ASSERT_NE(graaf.geefTakdata(0, 1), nullptr);
+    EXPECT_EQ((*graaf.geefTakdata(0, 1)), content);
+}
+
+TEST(graaf_met_knoopdata_test, constructor_with_default_parameter)
+{
+    GraafMetKnoopdata<ONGERICHT, int> graaf;
+
+    EXPECT_EQ(graaf.aantalKnopen(), 0);
+    EXPECT_EQ(graaf.aantalVerbindingen(), 0);
+}
+
+TEST(graaf_met_knoopdata_test, voeg_knoop_toe_no_parameter)
+{
+    GraafMetKnoopdata<ONGERICHT, std::string> graaf;
+
+    int knoop_nr = graaf.voegKnoopToe();
+
+    ASSERT_NE(graaf.geefKnoopdata(knoop_nr), nullptr);
+    EXPECT_EQ((*graaf.geefKnoopdata(knoop_nr)), std::string());
+}
+
+TEST(graaf_met_knoopdata_test, voeg_knoop_toe)
+{
+    GraafMetKnoopdata<ONGERICHT, std::string> graaf;
+
+    const std::string content = "Dit is een test";
+
+    int knoop_nr = graaf.voegKnoopToe(content);
+
+    ASSERT_NE(graaf.geefKnoopdata(knoop_nr), nullptr);
+    EXPECT_EQ((*graaf.geefKnoopdata(knoop_nr)), content);
+}
+
+TEST(graaf_met_knoopen_takdata_test, constructor_with_default_parameter)
+{
+    GraafMetKnoopEnTakdata<ONGERICHT, int, int> graaf;
+
+    EXPECT_EQ(graaf.aantalKnopen(), 0);
+    EXPECT_EQ(graaf.aantalVerbindingen(), 0);
+}
+

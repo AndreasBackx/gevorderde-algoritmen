74cb6df7c895da8908d4d8dd4ad8b6df066fd284
MartenBE <martijn.saelens@protonmail.com>
2018-06-15 14:54:31 +0200

    Added clang-format

--- generic-project-files/generate_makefile.sh
+++ generic-project-files/generate_makefile.sh
@@ -12,17 +12,18 @@ cd build
 
 if [ -x "$(command -v clang)" ]
 then
-    cmake .. \
-        -DCMAKE_C_COMPILER=clang \
-        -DCMAKE_CXX_COMPILER=clang++ \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-    cp compile_commands.json ..
+    cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+    # ln -s compile_commands.json .. # cmake compile_commands.json generation does not generate info for header files
+    # use compdb instead until this is fixed (https://github.com/Sarcasm/compdb)
+    if [ -x "$(command -v compdb)" ]
+    then
+        compdb list > ../compile_commands.json
+    else
+        echo "Warning: compdb is not installed" >&2
+    fi
 else
     cmake ..
-    echo "Warning: clang is not installed, using default compiler." >&2
+    echo "Warning: clang is not installed, using default compiler" >&2
 fi
 
-# Use the following commands to compile and debug (no need to run cmake everytime)
-#
-# scan-build make
-# make test
